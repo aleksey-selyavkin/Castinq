361d880c69d75b436af79a5f193b7ca7655e88e4
Aleksey <aleksey.selyavkin@gmail.com>
2016-11-27 13:43:52 +0200

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+/*
+ * Bower party (from bower_components)
+ */
+//= ../../bower_components/jquery/dist/jquery.js
+
+//= ../../bower_components/OwlCarousel2/dist/owl.carousel.js
+
+
+/*
+ * Custom (from src/js/partials)
+ */
+//= partials/app.js
---
+++ src/js/partials/app.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+
+	/* header's height */
+	$(document).scroll(function() {
+		var changePoint = $(".header").offset().top;
+		if (changePoint > 100) {
+			$(".header").css("height", "65px");
+			$(".header-nav a").css("line-height", "61px");
+		} else {
+			$(".header").css("height", "");
+			$(".header-nav a").css("line-height", "");
+		}
+	});
+
+	/* Smooth scrolling */
+	$("a.scroll").click(function(event) {
+		event.preventDefault();
+		var elementClick = $(this).attr("href");
+		var destination = $(elementClick).offset().top;
+		$("html,body").animate({
+			scrollTop: destination
+		}, 1300);
+		return false;
+	});
+
+	/* sliders */
+	$('#header-slider').owlCarousel({
+		items: 1,
+		loop: true,
+		// autoplay: true,
+		autoplayTimeout: 8000,
+		autoplayHoverPause: true,
+		smartSpeed: 1500
+	});
+	$('#gigs-slider, #posts-slider').owlCarousel({
+		loop: true,
+		nav: true,
+		navText: "",
+		margin: 20,
+		smartSpeed: 500,
+		responsive:{
+			0: {
+				items:1
+			},
+			768: {
+				items:2
+			},
+			992: {
+				items:3
+			}
+		}
+	});
+
+});
